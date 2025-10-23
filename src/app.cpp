1a9879349f90a9cc03f9fbd1a28ef8304f807517
Bernardo Sabino <contatobernardosabino@gmail.com>
2025-10-23 19:16:04 -0300

    feat: agendamentoController, agendamentoService

--- src/app.cpp
+++ src/app.cpp
@@ -1,26 +1,38 @@
 #include "app.hpp"
-
 #include "consoleUI.hpp"
 
-
+/**
+ * @brief Construtor do App.
+ * Inicializa todos os membros na ordem exata em que foram declarados no app.hpp.
+ */
 App::App()
     : connection(),
       alunoMapper(),
       professorMapper(),
       bus(),
-      agendamentoService(connection, bus),
+      sessionManager(bus),
       horarioService(connection, bus),
+      agendamentoService(connection, bus, horarioService),
       alunoService(connection, bus, alunoMapper, agendamentoService),
       professorService(connection, bus, professorMapper, horarioService),
-      horarioController(horarioService),
-      sessionManager(bus),
       alunoController(alunoService),
       professorController(professorService),
-      loginController(alunoService, professorService, bus) {}
+      horarioController(horarioService),
+      loginController(alunoService, professorService, bus),
+      agendamentoController(agendamentoService)
+{
+}
+
 
 void App::run() {
-    ConsoleUI ui(alunoController, professorController, loginController, horarioController,
-                 sessionManager);
+    ConsoleUI ui(
+        alunoController,
+        professorController,
+        loginController,
+        horarioController,
+        sessionManager,
+        agendamentoController
+    );
 
     ui.start();
 }
