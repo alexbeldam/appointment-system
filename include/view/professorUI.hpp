d7a2f72501ebab1c94ab9e8cc4a058d0c948678c
MirellaABRS <miialmeidaa24@gmail.com>
2025-11-04 13:24:26 -0300

    feat: atualização de perfil

--- include/view/professorUI.hpp
+++ include/view/professorUI.hpp
@@ -5,6 +5,8 @@
 #include "controller/horarioController.hpp"
 #include "controller/professorController.hpp"
 #include "view/consoleUI.hpp"
+#include "event/bus.hpp"           
+#include "service/sessionManager.hpp"
 
 /**
  * @brief Classe da Interface de Usuário (Presentation Layer) para Professores
@@ -19,6 +21,8 @@ class ProfessorUI : public ConsoleUI {
     const ProfessorController& professorController;
     const HorarioController& horarioController;
     const AgendamentoController& agendamentoController;
+    EventBus& bus;
+    SessionManager& sessionManager;
 
     /**
      * @brief Gerencia o fluxo de I/O e delega o cadastro de um novo horário
@@ -42,9 +46,15 @@ class ProfessorUI : public ConsoleUI {
      */
     void excluir_todos_horarios() const;
 
+     /**
+     * @brief Atualiza o perfil do professor logado.
+     */
+    void atualizar_perfil();
+
    public:
     ProfessorUI(const ProfessorController& pc, const HorarioController& hc,
-                const AgendamentoController& ac, SessionManager& sm);
+                const AgendamentoController& ac, EventBus& bus,
+                SessionManager& sm);
 
     virtual bool show() const override;
 };
